413b4e6e101adede6ca73346cf1eac8ea2633814
Chris Nelson <superchrisnelson@gmail.com>
2019-02-25 18:09:11 -0500

    first failing test

--- test/bowling-scorer.test.js
+++ test/bowling-scorer.test.js
@@ -3,15 +3,9 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../src/bowling-scorer';
 
 describe('<bowling-scorer>', () => {
-  it('has a default property header', async () => {
+  it('displays an initial score of 0', async () => {
     const el = await fixture('<bowling-scorer></bowling-scorer>');
-    expect(el.title).to.equal('open-wc');
+    expect(el.shadowRoot.innerHTML).to.contain('Score: 0');
   });
 
-  it('allows property header to be overwritten', async () => {
-    const el = await fixture(html`
-      <bowling-scorer title="different"></bowling-scorer>
-    `);
-    expect(el.title).to.equal('different');
-  });
 });
