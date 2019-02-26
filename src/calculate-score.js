ffd0223ff6e61f869ec9dcb1de8c9c1c30793602
Chris Nelson <superchrisnelson@gmail.com>
2019-02-26 17:31:38 -0500

    naive spares implementation (passes test)

--- src/calculate-score.js
+++ src/calculate-score.js
@@ -1,3 +1,9 @@
-const calculateScore = (rolls) => rolls.reduce((total, roll) => total + roll, 0)
+const calculateScore = (rolls) => {
+  if (rolls.length > 2 && rolls[0] + rolls[1] === 10) {
+    return rolls[0] + rolls[1] + (2 * rolls[2]);
+  } else {
+    return rolls.reduce((total, roll) => total + roll, 0);
+  }
+}
 
 export default calculateScore
